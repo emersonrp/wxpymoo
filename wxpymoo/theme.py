ed29a6cdec08cdd94b7462c7632cefcc4b8bc6f6
Russell Pickett <emerson@craigslist.org>
2016-01-14 19:13:42 -0800

    Hard-coded theme + ANSI working

--- wxpymoo/theme.py
+++ wxpymoo/theme.py
@@ -1,37 +1,32 @@
-package WxMOO::Theme;
-use strict;
-use warnings;
-use Wx qw(:colour);
-use v5.14;
+import wx
 
-sub new {
-    my ($class, %args) = @_;
-    bless {
-        b_black   => '002b36',
-        d_black   => '073642',
-        b_red     => 'cb4b16',
-        d_red     => 'dc322f',
-        b_green   => '586e75',
-        d_green   => '859900',
-        d_yellow  => '657b83',
-        b_yellow  => 'b58900',
-        b_blue    => '839496',
-        d_blue    => '268bd2',
-        b_magenta => '6c71c4',
-        d_magenta => 'd33682',
-        b_cyan    => '93a1a1',
-        d_cyan    => '2aa198',
-        b_white   => 'fdf6e3',
-        d_white   => 'eee8d5',
-        %args,
-    }, $class;
+solarized = {
+    'b_black'   : '#002b36',
+    'd_black'   : '#073642',
+    'b_red'     : '#cb4b16',
+    'd_red'     : '#dc322f',
+    'b_green'   : '#586e75',
+    'd_green'   : '#859900',
+    'd_yellow'  : '#657b83',
+    'b_yellow'  : '#b58900',
+    'b_blue'    : '#839496',
+    'd_blue'    : '#268bd2',
+    'b_magenta' : '#6c71c4',
+    'd_magenta' : '#d33682',
+    'b_cyan'    : '#93a1a1',
+    'd_cyan'    : '#2aa198',
+    'b_white'   : '#fdf6e3',
+    'd_white'   : '#eee8d5',
 }
 
-sub Colour {
-    my ($self, $color, $brightness) = @_;
-    $brightness = $brightness ? 'b' : 'd';
-    my @rgb = unpack 'C*', pack 'H*', $self->{"${brightness}_$color"};
-    return Wx::Colour->new(@rgb);
-}
+class Theme(dict):
+    def __init__(self, *init):
+        global solarized
+        for k in solarized:
+            self[k] = solarized[k]
+        for k in init:
+            self[k] = init[k]
 
-1;
+    def Colour(self, colour, bright):
+        prefix = 'b' if bright else 'd'
+        return self[prefix + "_" + colour]
--- wxpymoo/window/outputpane.py
+++ wxpymoo/window/outputpane.py
@@ -3,6 +3,7 @@ import wx.richtext
 
 import wxpymoo.prefs as prefs
 import wxpymoo.utility
+from wxpymoo.theme import Theme
 
 import webbrowser
 
@@ -10,7 +11,6 @@ import re
 
 # TODO we need a better output_filter scheme, probably?
 #use WxMOO.MCP21
-#use WxMOO.Theme
 
 class OutputPane(wx.richtext.RichTextCtrl):
 
@@ -21,13 +21,20 @@ class OutputPane(wx.richtext.RichTextCtrl):
         )
         self.input_pane = connection.input_pane
 
-        self.restyle_thyself()
+        # state toggles for ANSI processing
+        self.bright  = False
+        self.inverse = False
 
-        self.Bind(wx.EVT_SET_FOCUS, self.focus_input)
-        self.Bind(wx.EVT_TEXT_URL,  self.process_url_click)
+        self.theme = Theme()
+
         # TODO - this probably should be a preference, but for now, this is the
         # least-bad default behavior.
         self.Bind(wx.EVT_SIZE,      self.scroll_to_bottom)
+        self.Bind(wx.EVT_SET_FOCUS, self.focus_input)
+        self.Bind(wx.EVT_TEXT_URL,  self.process_url_click)
+
+        self.restyle_thyself()
+
 
     def scroll_to_bottom(self, evt):
         self.ShowPosition(self.GetLastPosition())
@@ -73,9 +80,9 @@ class OutputPane(wx.richtext.RichTextCtrl):
             if (True or prefs.get('use_ansi')):
                 stuff = self.ansi_parse(line)
                 for bit in stuff:
-                    #if (bit):
-                        #self.apply_ansi(bit)
-                    #else:
+                    if type(bit) is list:
+                        self.apply_ansi(bit)
+                    else:
                         # TODO - this might should be separate from use_ansi.
                         # TODO - snip URLs first then ansi-parse pre and post?
                         if prefs.get('highlight_urls'):
@@ -85,8 +92,7 @@ class OutputPane(wx.richtext.RichTextCtrl):
                                 if re.match(wxpymoo.utility.URL_REGEX, chunk):
                                     self.BeginURL(chunk)
                                     self.BeginUnderline()
-                                    #self.BeginTextColour( self.lookup_color('blue', True) )
-                                    self.BeginTextColour( wx.BLUE )
+                                    self.BeginTextColour( self.lookup_colour('blue', True) )
 
                                     self.WriteText(chunk)
 
@@ -106,55 +112,48 @@ class OutputPane(wx.richtext.RichTextCtrl):
 
     def focus_input(self,evt): self.input_pane.SetFocus()
 
-    #theme = wxpymoo.theme.Theme()
-
-    def lookup_color(self, color, bright):
-        #return theme.Colour(color, True if self.bright else False)
-        pass
+    def lookup_colour(self, color, *bright):
+        return self.theme.Colour(color,
+                True if (bright or self.bright) else False)
 
     def apply_ansi(self, bit):
-        (type, payload) = bit
-#        if (type == 'control'):
-#            given (payload) {
-#                when ('normal')       {
-#                    my plain_style = Wx.TextAttr.new
-#                    if (self.{'inverse'}) {
-#                        say STDERR "I'm inverse!"
-#                        self.invert_colors
-#                    }
-#                    self.SetDefaultStyle(plain_style)
-#                }
-#                when ('bold')         { self.BeginBold;     }
-#                when ('dim')          { self.EndBold;       } # TODO - dim further than normal?
-#                when ('underline')    { self.BeginUnderline }
-#                when ('blink')     {
-#                    # TODO - create timer
-#                    # apply style name
-#                    # periodically switch foreground color to background
-#                }
-#                when ('inverse')      { self.invert_colors; }
-#                when ('hidden')       { 1; }
-#                when ('strikethru')   { 1; }
-#                when ('no_bold')      { self.EndBold; }
-#                when ('no_underline') { self.EndUnderline }
-#                when ('no_blink')  {
-#                    # TODO - remove blink-code-handles style
-#                }
-#                when ('no_strikethru') { 1; }
-#            }
-#        } elsif (type eq 'foreground') {
-#            self.BeginTextColour(self.lookup_color(payload))
-#        } elsif (type eq "background") {
-#            my bg_attr = Wx.TextAttr.new
-#            bg_attr.SetBackgroundColour(self.lookup_color(payload))
-#            self.SetDefaultStyle(bg_attr)
-#            # self.BeginBackgroundColour(self.lookup_color(payload))
-#        } else {
-#            say STDERR "unknown ANSI type type"
-#        }
+        type, payload = bit
+        if type == 'control':
+            if payload == 'normal':
+                plain_style = wx.richtext.RichTextAttr()
+                if self.inverse: self.invert_colors()
+                self.SetDefaultStyle(plain_style)
+            elif payload == 'bold':      self.BeginBold()
+            elif payload == 'dim':       self.EndBold()    # TODO - dim further than normal?
+            elif payload == 'underline': self.BeginUnderline()
+            elif payload == 'blink':
+                pass
+                # TODO - create timer
+                # apply style name
+                # periodically switch foreground color to background
+            elif payload == 'inverse':      self.invert_colors()
+            elif payload == 'hidden':       pass
+            elif payload == 'strikethru':   pass
+            elif payload == 'no_bold':      self.EndBold()
+            elif payload == 'no_underline': self.EndUnderline()
+            elif payload == 'no_blink':
+                pass
+                # TODO - remove blink-code-handles style
+            elif payload == 'no_strikethru': pass
+
+        elif type == 'foreground':
+            self.BeginTextColour(self.lookup_colour(payload))
+        elif type == "background":
+            bg_attr = wx.TextAttr()
+            bg_attr.SetBackgroundColour(self.lookup_colour(payload))
+            self.SetDefaultStyle(bg_attr)
+            self.BeginBackgroundColour(self.lookup_colour(payload))
+        else:
+            print("unknown ANSI type:", type)
 
     def invert_colors(self):
-        current = self.GetStyle(self.GetInsertionPoint())
+        current = wx.richtext.RichTextAttr()
+        self.GetStyle(self.GetInsertionPoint(), current)
         fg = current.GetTextColour()
         bg = current.GetBackgroundColour()
         # TODO - hrmn current bg color seems to be coming out wrong.
@@ -163,62 +162,66 @@ class OutputPane(wx.richtext.RichTextCtrl):
         current.SetBackgroundColour(fg)
 
         self.inverse = False if self.inverse else True
-        # self.SetDefaultStyle(current);  # commenting this out until bg color confusion is resolved
-
-    ansi_codes = {
-            0     : [ 'control' , 'normal'        ],
-            1     : [ 'control' , 'bold'          ],
-            2     : [ 'control' , 'dim'           ],
-            4     : [ 'control' , 'underline'     ],
-            5     : [ 'control' , 'blink'         ],
-            7     : [ 'control' , 'inverse'       ],
-            8     : [ 'control' , 'hidden'        ],
-            9     : [ 'control' , 'strikethru'    ],
-            22    : [ 'control' , 'no_bold'       ], # normal font weight also cancels 'dim'
-            24    : [ 'control' , 'no_underline'  ],
-            25    : [ 'control' , 'no_blink'      ],
-            29    : [ 'control' , 'no_strikethru' ],
-            30    : [ 'foreground' , 'black'  ],
-            31    : [ 'foreground' , 'red'    ],
-            32    : [ 'foreground' , 'green'  ],
-            33    : [ 'foreground' , 'yellow' ],
-            34    : [ 'foreground' , 'blue'   ],
-            35    : [ 'foreground' , 'magenta'],
-            36    : [ 'foreground' , 'cyan'   ],
-            37    : [ 'foreground' , 'white'  ],
-
-            40    : [ 'background' , 'black'  ],
-            41    : [ 'background' , 'red'    ],
-            42    : [ 'background' , 'green'  ],
-            43    : [ 'background' , 'yellow' ],
-            44    : [ 'background' , 'blue'   ],
-            45    : [ 'background' , 'magenta'],
-            46    : [ 'background' , 'cyan'   ],
-            47    : [ 'background' , 'white'  ],
-    }
+        self.SetDefaultStyle(current);  # commenting this out until bg color confusion is resolved
 
     def ansi_parse(self, line):
-        #if (my beepcount = line =~ s/\007//g) {
-        #    for (1..beepcount) {
-        #        say STDERR "found a beep"
-        #        Wx.Bell();  # TODO -- "if beep is enabled in the prefs"
-        #    }
-        #}
+        global ansi_codes
+        beep_cleaned, count = re.subn("\007", '', line)
 
-        bits = re.split('\e\[(\d+(?:;\d+)*)m', line)
+        if count:
+            line = beep_cleaned
+            for b in range(0, count):
+                print("found a beep")
+                wx.Bell();  # TODO -- "if beep is enabled in the prefs"
 
-        #my @styled_text
-        #while (my (i, val) = each @bits) {
-        #    if (i % 2) {
-        #        for my c (split /;/, val) {
-        #            if (my style = ansi_codes{val}) {
-        #                push @styled_text, style
-        #            }
-        #        }
-        #    } else {
-        #        push @styled_text, val if val
-        #    }
-        #}
-        #return [@styled_text]
-        return [line]
+        styled_text = []
+
+        bits = re.split('\x1b\[(\d+(?:;\d+)*)m', line)
+
+        if len(bits) > 1:
+            it = iter(bits)
+            for text, ansi in zip(it, it):
+                if text != '': styled_text.append(text)
+                codes = ansi.split(';')
+                for code in codes:
+                    styled_text.append(ansi_codes[int(code)])
+        else:
+            styled_text.append(bits[0])
+
+        # split() done et our linefeed yo?
+        if len(styled_text) > 1: styled_text.append('\n')
+
+        return styled_text
+
+ansi_codes = {
+    0     : [ 'control' , 'normal'        ],
+    1     : [ 'control' , 'bold'          ],
+    2     : [ 'control' , 'dim'           ],
+    4     : [ 'control' , 'underline'     ],
+    5     : [ 'control' , 'blink'         ],
+    7     : [ 'control' , 'inverse'       ],
+    8     : [ 'control' , 'hidden'        ],
+    9     : [ 'control' , 'strikethru'    ],
+    22    : [ 'control' , 'no_bold'       ], # normal font weight also cancels 'dim'
+    24    : [ 'control' , 'no_underline'  ],
+    25    : [ 'control' , 'no_blink'      ],
+    29    : [ 'control' , 'no_strikethru' ],
+    30    : [ 'foreground' , 'black'  ],
+    31    : [ 'foreground' , 'red'    ],
+    32    : [ 'foreground' , 'green'  ],
+    33    : [ 'foreground' , 'yellow' ],
+    34    : [ 'foreground' , 'blue'   ],
+    35    : [ 'foreground' , 'magenta'],
+    36    : [ 'foreground' , 'cyan'   ],
+    37    : [ 'foreground' , 'white'  ],
+
+    40    : [ 'background' , 'black'  ],
+    41    : [ 'background' , 'red'    ],
+    42    : [ 'background' , 'green'  ],
+    43    : [ 'background' , 'yellow' ],
+    44    : [ 'background' , 'blue'   ],
+    45    : [ 'background' , 'magenta'],
+    46    : [ 'background' , 'cyan'   ],
+    47    : [ 'background' , 'white'  ],
+}
 
