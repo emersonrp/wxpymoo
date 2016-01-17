8325a804fbae247a479c8c2498f30915598eba3a
Russell Pickett <emerson@craigslist.org>
2016-01-16 23:00:30 -0800

    import stuff after making wx.App() to crush warnings

--- wxpymoo.py
+++ wxpymoo.py
@@ -5,19 +5,22 @@ from twisted.internet import wxreactor
 wxreactor.install() # must be before 'import reactor'
 from twisted.internet import reactor
 
-import wxpymoo.prefs
-from wxpymoo.window.main import Main
 
 def run():
     app = wx.App(False)
 
-    reactor.registerWxApp(app)
+    from wxpymoo.window.main import Main
+    import wxpymoo.prefs as prefs
 
-    wxpymoo.prefs.Initialize()
+    prefs.Initialize()
     frame = Main(None, "wxpymoo")
     frame.Show(True)
+
+    reactor.registerWxApp(app)
     reactor.run()
+
     app.MainLoop()
+
 
 if __name__ == "__main__":
     run()
