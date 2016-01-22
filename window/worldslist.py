21c8207be340608f99247ec5f8e838af66dc7ddd
Russell Pickett <emerson@craigslist.org>
2016-01-22 15:22:01 -0800

    Work on worldslist
    -- simplify / compact the number of controls
    -- (temporarily?) remove the 'notes' box
    -- make the description box dtrt with <a> links
    -- disable new/reset/save for now

--- window/worldslist.py
+++ window/worldslist.py
@@ -4,6 +4,12 @@ import wx.html
 import prefs
 from worlds import worlds
 
+import webbrowser
+
+class DescriptionBox(wx.html.HtmlWindow):
+    def OnLinkClicked(self, link):
+        webbrowser.open(link.GetHref())
+
 class WorldsList(wx.Dialog):
 
     def __init__(self, parent):
@@ -11,37 +17,113 @@ class WorldsList(wx.Dialog):
 
         self.connection = parent.connection
 
-        world_details_staticbox = wx.StaticBox(self)
-        world_details_box       = wx.StaticBoxSizer(world_details_staticbox, wx.HORIZONTAL)
-
-        world_label  = wx.StaticText(self, label = "World")
-        self.world_picker = wx.Choice(self, style = wx.CB_SORT )
+        worlds_label      = wx.StaticText(self, label = "World:")
+        self.world_picker = wx.Choice(self, style     = wx.CB_SORT )
 
         for world in worlds: self.world_picker.Append(world)
 
-        world_picker_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        world_picker_sizer.Add(world_label,  0,         wx.ALIGN_CENTER_VERTICAL, 0)
-        world_picker_sizer.Add(self.world_picker, 1, wx.LEFT|wx.ALIGN_CENTER_VERTICAL, 5)
+        host_label = wx.StaticText(self, label = "Host:")
+        port_label = wx.StaticText(self, label = "Port:")
 
-        self.world_details_panel = WorldPanel(self)
-        world_details_box.Add(self.world_details_panel, 1, wx.EXPAND, 0)
+        self.host = wx.TextCtrl(self)
 
-        button_sizer = self.CreateButtonSizer( wx.OK | wx.CANCEL )
+        # stick port + type into their own boxsizer
+        self.port = wx.SpinCtrl(self)
+        self.port.SetRange(0, 65535)
+        self.port.SetValue(7777)
+        self.conntype = wx.Choice (self, choices = ['Direct','SSL','SSH Fwd'] )
+        self.conntype.SetSelection(0)
+        port_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        port_sizer.Add(self.port,     1, wx.EXPAND)
+        port_sizer.Add([5,5],         0, wx.EXPAND)
+        port_sizer.Add(self.conntype, 0, wx.EXPAND)
+
+        username_label = wx.StaticText(self, label = "Username:")
+        password_label = wx.StaticText(self, label = "Password:")
+        self.username  = wx.TextCtrl(self)
+        self.password  = wx.TextCtrl(self, style = wx.TE_PASSWORD)
+
+        self.ssh_username_label = wx.StaticText(self, label = "SSH User:")
+        self.ssh_loc_host_label = wx.StaticText(self, label = "SSH Host:")
+        self.ssh_username       = wx.TextCtrl(self)
+        self.ssh_loc_host       = wx.TextCtrl(self)
+
+        field_sizer = wx.FlexGridSizer(cols = 2, hgap = 5, vgap = 5)
+        field_sizer.AddMany([
+            (worlds_label           , 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.world_picker      , 0, wx.EXPAND                              , 0),
+            (host_label             , 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.host              , 0, wx.EXPAND                              , 0),
+            (port_label             , 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (port_sizer             , 0, wx.EXPAND                              , 0),
+            (username_label         , 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.username          , 0, wx.EXPAND                              , 0),
+            (password_label         , 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.password          , 0, wx.EXPAND                              , 0),
+            (self.ssh_username_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.ssh_username      , 0, wx.EXPAND                              , 0),
+            (self.ssh_loc_host_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0),
+            (self.ssh_loc_host      , 0, wx.EXPAND                              , 0),
+        ])
+        field_sizer.AddGrowableCol(1)
+
+        self.desc = DescriptionBox(self, style = wx.BORDER_NONE)
+        desc_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "Description:"), wx.VERTICAL)
+        desc_box.Add(self.desc, 1, wx.EXPAND)
+
+        self.note = wx.TextCtrl(self, style = wx.TE_MULTILINE|wx.BORDER_NONE)
+        #note_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "Notes:"), wx.VERTICAL)
+        #note_box.Add(self.note, 1, wx.EXPAND)
+
+        mcp_check          = wx.CheckBox(self, label = "MCP 2.1")
+        login_dialog_check = wx.CheckBox(self, label = "Use Login Dialog")
+        shortlist_check    = wx.CheckBox(self, label = "On Short List")
+        checkbox_sizer     = wx.GridSizer(3, 2, 0, 0)
+        checkbox_sizer.AddMany([
+            (mcp_check         , 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5),
+            (login_dialog_check, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5),
+            (shortlist_check   , 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5),
+        ])
+
+        new_button   = wx.Button(self, label = "New")
+        reset_button = wx.Button(self, label = "Reset")
+        save_button  = wx.Button(self, label = "Save")
+        new_button.Disable()
+        reset_button.Disable()
+        save_button.Disable()
+        button_sizer = wx.FlexGridSizer(cols = 3, rows = 1, hgap = 0, vgap = 0)
+        button_sizer.AddMany ([
+            (new_button  , 0, wx.ALL|wx.ALIGN_RIGHT, 5),
+            (reset_button, 0, wx.ALL               , 5),
+            (save_button , 0, wx.ALL               , 5),
+        ])
+        button_sizer.AddGrowableCol(0)
+
+        world_details_staticbox = wx.StaticBox(self)
+        self.world_details_box  = wx.StaticBoxSizer(world_details_staticbox, wx.VERTICAL)
+        self.world_details_box.AddMany ([
+            (field_sizer   , 0, wx.ALL|wx.EXPAND, 5),
+            (desc_box      , 1, wx.ALL|wx.EXPAND, 5),
+            #(note_box      , 1, wx.ALL|wx.EXPAND, 5),
+            (checkbox_sizer, 0, wx.EXPAND       , 5),
+            (button_sizer  , 0, wx.EXPAND       , 0),
+        ])
+
+        main_button_sizer = self.CreateButtonSizer( wx.OK | wx.CANCEL )
 
         # Hax: change the "OK" button to "Connect"
         # This is a hoop-jumping exercise to use the platform-specific locations
         # of "OK" and "Cancel" instead of the hoop-jumping exercise of making my
         # own buttons.  There's almost certainly a better way to do this.
-        for b in button_sizer.GetChildren():
+        for b in main_button_sizer.GetChildren():
             bwin = b.GetWindow()
             if (not bwin) or (bwin.GetLabel() != '&OK'): continue
             bwin.SetLabel('&Connect')
             break
 
         main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.Add(world_picker_sizer, 0, wx.EXPAND | wx.ALL,            10)
-        main_sizer.Add(world_details_box,  1, wx.EXPAND | wx.LEFT | wx.RIGHT, 10)
-        main_sizer.Add(button_sizer,       0, wx.EXPAND | wx.ALL,            10)
+        main_sizer.Add(self.world_details_box, 1, wx.EXPAND | wx.LEFT | wx.RIGHT, 10)
+        main_sizer.Add(main_button_sizer     , 0, wx.EXPAND | wx.ALL            , 5)
 
         last_world_name = prefs.get('last_world')
         last_world = self.world_picker.FindString(last_world_name)
@@ -51,136 +133,55 @@ class WorldsList(wx.Dialog):
             last_world = self.world_picker.FindString(last_world_name)
 
         self.world_picker.SetSelection(last_world)
-        self.world_details_panel.fill_thyself(worlds[last_world_name])
+        self.fill_thyself(worlds[last_world_name])
 
-        self.SetSizer(main_sizer)
-        main_sizer.Fit(self)
+        self.SetSizerAndFit(main_sizer)
         self.Layout()
 
         self.Centre(wx.BOTH)
 
         self.Bind(wx.EVT_CHOICE, self.select_world, self.world_picker)
+        self.Bind(wx.EVT_CHOICE, self.show_ssh_fields_if_appropriate, self.conntype)
         self.Bind(wx.EVT_BUTTON, self.on_connect, id = wx.ID_OK)
+
+        self.show_ssh_fields_if_appropriate()
 
 
     def select_world(self, evt):
         world = worlds[self.world_picker.GetStringSelection()]
-        self.world_details_panel.fill_thyself(world)
+        self.fill_thyself(world)
 
-    # TODO - make WxMOO.World have a notion of "connect to yourself"
-    # Also therefore merge WxMOO.World and WxMOO.Window.WorldPanel
+    # TODO - make wxpymoo.World have a notion of "connect to yourself"
+    # Also therefore merge wxpymoo.world and wxpymoo.window.world
     def on_connect(self, evt):
-        self.connection.connect(self.world_details_panel.world)
+        self.connection.connect(self.world)
         self.Hide()
 
-
-class WorldPanel(wx.Panel):
-
-    import prefs
-
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
-
-        name_label = wx.StaticText(self, label = "Name:")
-        host_label = wx.StaticText(self, label = "Host:")
-        port_label = wx.StaticText(self, label = "Port:")
-        self.name = wx.TextCtrl(self)
-        self.host = wx.TextCtrl(self)
-        self.port = wx.SpinCtrl(self)
-        self.port.SetRange(0, 65535)
-        self.port.SetValue(7777)
-
-        username_label = wx.StaticText(self, label = "Username:")
-        password_label = wx.StaticText(self, label = "Password:")
-        type_label = wx.StaticText(self, label = "Type:")
-        self.username = wx.TextCtrl(self)
-        self.password = wx.TextCtrl(self, style = wx.TE_PASSWORD)
-        self.conntype = wx.Choice  (self, choices = ['Socket','SSL','SSH Forwarding'] )
-        self.conntype.SetSelection(0)
-
-        self.ssh_username_label = wx.StaticText(self, label = "SSH Username:")
-        self.ssh_loc_host_label = wx.StaticText(self, label = "SSH Host:")
-        self.ssh_username       = wx.TextCtrl(self)
-        self.ssh_loc_host       = wx.TextCtrl(self)
-
-        field_sizer = wx.FlexGridSizer(cols = 2, hgap = 5, vgap = 10)
-        field_sizer.Add(name_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.name, 0, wx.EXPAND, 0)
-        field_sizer.Add(host_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.host, 0, wx.EXPAND, 0)
-        field_sizer.Add(port_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.port, 0, wx.EXPAND, 0)
-        field_sizer.Add(username_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.username, 0, wx.EXPAND, 0)
-        field_sizer.Add(password_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.password, 0, wx.EXPAND, 0)
-        field_sizer.Add(type_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.conntype, 0, wx.EXPAND, 0)
-        field_sizer.Add(self.ssh_username_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.ssh_username, 0, wx.EXPAND, 0)
-        field_sizer.Add(self.ssh_loc_host_label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 0)
-        field_sizer.Add(self.ssh_loc_host, 0, wx.EXPAND, 0)
-        field_sizer.AddGrowableCol(1)
-
-        self.desc = wx.html.HtmlWindow(self)
-        self.desc.SetBorders(0)
-        desc_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "description"), wx.HORIZONTAL)
-        desc_box.Add(self.desc, 1, wx.EXPAND, 0)
-
-        self.note = wx.TextCtrl(self, style = wx.TE_MULTILINE)
-        note_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "notes"), wx.HORIZONTAL)
-        note_box.Add(self.note, 1, wx.EXPAND, 0)
-
-        mcp_check          = wx.CheckBox(self, label = "MCP 2.1")
-        login_dialog_check = wx.CheckBox(self, label = "Use Login Dialog")
-        shortlist_check    = wx.CheckBox(self, label = "On Short List")
-        checkbox_sizer = wx.GridSizer(3, 2, 0, 0)
-        checkbox_sizer.Add(mcp_check, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        checkbox_sizer.Add(login_dialog_check, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        checkbox_sizer.Add(shortlist_check, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-
-        new_button   = wx.Button(self, label = "New")
-        reset_button = wx.Button(self, label = "Reset")
-        save_button  = wx.Button(self, label = "Save")
-        button_sizer = wx.FlexGridSizer(cols = 3, rows = 1, hgap = 0, vgap = 0)
-        button_sizer.Add(new_button, 0, wx.ALL|wx.ALIGN_RIGHT, 5)
-        button_sizer.Add(reset_button, 0, wx.ALL, 5)
-        button_sizer.Add(save_button, 0, wx.ALL, 5)
-        button_sizer.AddGrowableCol(0)
-
-        self.panel_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.panel_sizer.Add(field_sizer, 0, wx.ALL|wx.EXPAND, 10)
-        self.panel_sizer.Add(desc_box, 1, wx.EXPAND, 0)
-        self.panel_sizer.Add(note_box, 1, wx.EXPAND, 0)
-        self.panel_sizer.Add(checkbox_sizer, 0, wx.EXPAND, 0)
-        self.panel_sizer.Add(button_sizer, 0, wx.EXPAND, 0)
-
-        self.SetSizerAndFit(self.panel_sizer)
-
-        self.Bind(wx.EVT_CHOICE, self.show_ssh_fields_if_appropriate)
-
-        self.show_ssh_fields_if_appropriate()
-
-    def on_save(self, evt): pass
-
+    def on_save (self, evt): pass
     def on_reset(self, evt): pass
-
-    def on_new(self, evt): pass
+    def on_new  (self, evt): pass
 
     def fill_thyself(self, world):
         self.world = world
 
-        self.name.SetValue(unicode(world.get("name")))
-        if world.get('host')        : self.host.SetValue(unicode(world.get("host")))
-        if world.get('port')        : self.port.SetValue(int(world.get("port")))
-        if world.get('username')    : self.username.SetValue(world.get("username"))
-        if world.get('password')    : self.password.SetValue(world.get("password"))
-        if world.get('description') : self.desc.SetPage(world.get('description'))
-        if world.get('note')        : self.note.SetValue(unicode(world.get("note")))
+        self.host.SetValue(unicode(world.get("host")   ) or "")
+        self.port.SetValue(    int(world.get("port")   ) or "")
         self.conntype.SetSelection(world.get("conntype") or 0)
+        self.username.SetValue(    world.get("username") or "")
+        self.password.SetValue(    world.get("password") or "")
 
-        if world.get('ssh_username'): self.ssh_username.SetValue(world.get("ssh_username"))
-        if world.get('ssh_loc_host'): self.ssh_loc_host.SetValue(world.get("ssh_loc_host"))
+        desc = unicode(world.get('description'))
+        if not desc or desc == "None": desc = ''
+        self.desc.SetPage(desc)
+        # TODO - figure out how to get the Right Size instead of hard-coded 200px
+        self.desc.SetMinSize((1, 200))
+
+        #note = unicode(world.get('note'))
+        #if not note or note == "None": note = ''
+        #self.note.SetValue(note)
+
+        self.ssh_username.SetValue(world.get("ssh_username") or "")
+        self.ssh_loc_host.SetValue(world.get("ssh_loc_host") or "")
 
         self.show_ssh_fields_if_appropriate()
 
@@ -191,4 +192,4 @@ class WorldPanel(wx.Panel):
         self.ssh_username.Show(to_show)
         self.ssh_loc_host.Show(to_show)
 
-        self.panel_sizer.Layout()
+        self.world_details_box.Layout()
