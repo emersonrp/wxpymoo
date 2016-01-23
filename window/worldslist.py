059423577b4e4f697f1e37327c42ba21c99441ff
Russell Pickett <emerson@craigslist.org>
2016-01-22 17:53:55 -0800

    Worlds dialog 'save' and 'reset' now working

--- window/worldslist.py
+++ window/worldslist.py
@@ -10,6 +10,8 @@ class DescriptionBox(wx.html.HtmlWindow):
     def OnLinkClicked(self, link):
         webbrowser.open(link.GetHref())
 
+conntypes = [ 'Direct', 'SSL', 'SSH Fwd' ]
+
 class WorldsList(wx.Dialog):
 
     def __init__(self, parent):
@@ -31,8 +33,7 @@ class WorldsList(wx.Dialog):
         self.port = wx.SpinCtrl(self)
         self.port.SetRange(0, 65535)
         self.port.SetValue(7777)
-        self.conntype = wx.Choice (self, choices = ['Direct','SSL','SSH Fwd'] )
-        self.conntype.SetSelection(0)
+        self.conntype = wx.Choice(self, choices = conntypes )
         port_sizer = wx.BoxSizer(wx.HORIZONTAL)
         port_sizer.Add(self.port,     1, wx.EXPAND)
         port_sizer.Add([5,5],         0, wx.EXPAND)
@@ -71,7 +72,7 @@ class WorldsList(wx.Dialog):
         desc_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "Description:"), wx.VERTICAL)
         desc_box.Add(self.desc, 1, wx.EXPAND)
 
-        self.note = wx.TextCtrl(self, style = wx.TE_MULTILINE|wx.BORDER_NONE)
+        #self.note = wx.TextCtrl(self, style = wx.TE_MULTILINE|wx.BORDER_NONE)
         #note_box = wx.StaticBoxSizer(wx.StaticBox(self, label = "Notes:"), wx.VERTICAL)
         #note_box.Add(self.note, 1, wx.EXPAND)
 
@@ -88,9 +89,7 @@ class WorldsList(wx.Dialog):
         new_button   = wx.Button(self, label = "New")
         reset_button = wx.Button(self, label = "Reset")
         save_button  = wx.Button(self, label = "Save")
-        new_button.Disable()
-        reset_button.Disable()
-        save_button.Disable()
+
         button_sizer = wx.FlexGridSizer(cols = 3, rows = 1, hgap = 0, vgap = 0)
         button_sizer.AddMany ([
             (new_button  , 0, wx.ALL|wx.ALIGN_RIGHT, 5),
@@ -98,6 +97,10 @@ class WorldsList(wx.Dialog):
             (save_button , 0, wx.ALL               , 5),
         ])
         button_sizer.AddGrowableCol(0)
+
+        self.Bind(wx.EVT_BUTTON, self.on_new, new_button)
+        self.Bind(wx.EVT_BUTTON, self.on_reset, reset_button)
+        self.Bind(wx.EVT_BUTTON, self.on_save, save_button)
 
         world_details_staticbox = wx.StaticBox(self)
         self.world_details_box  = wx.StaticBoxSizer(world_details_staticbox, wx.VERTICAL)
@@ -133,7 +136,8 @@ class WorldsList(wx.Dialog):
             last_world = self.world_picker.FindString(last_world_name)
 
         self.world_picker.SetSelection(last_world)
-        self.fill_thyself(worlds[last_world_name])
+        self.world = worlds[last_world_name]
+        self.fill_thyself()
 
         self.SetSizerAndFit(main_sizer)
         self.Layout()
@@ -148,8 +152,8 @@ class WorldsList(wx.Dialog):
 
 
     def select_world(self, evt):
-        world = worlds[self.world_picker.GetStringSelection()]
-        self.fill_thyself(world)
+        self.world = worlds[self.world_picker.GetStringSelection()]
+        self.fill_thyself()
 
     # TODO - make wxpymoo.World have a notion of "connect to yourself"
     # Also therefore merge wxpymoo.world and wxpymoo.window.world
@@ -157,16 +161,41 @@ class WorldsList(wx.Dialog):
         self.connection.connect(self.world)
         self.Hide()
 
-    def on_save (self, evt): pass
-    def on_reset(self, evt): pass
-    def on_new  (self, evt): pass
+    def on_save (self, evt):
+        world = self.world
 
-    def fill_thyself(self, world):
-        self.world = world
+        world['host'] = self.host.GetValue()
+        world['port'] = self.port.GetValue()
+        world['conntype'] = self.conntype.GetStringSelection()
+        world['username'] = self.username.GetValue()
+        world['password'] = self.password.GetValue()
+        world['desc'] = self.desc.GetOpenedPage()
+        #world['note'] = welf.note.GetValue()
+
+        if world['conntype'] == 2:  # ssh fwd
+            world['ssh_username'] = self.ssh_username.GetValue()
+            world['ssh_loc_host'] = self.ssh_loc_host.GetValue()
+        else:
+            world['ssh_username'] = ''
+            world['ssh_loc_host'] = ''
+
+        world.save() # TODO - error checking, like at all
+
+    def on_reset(self, evt):
+        # repopulate with the data from the World as last saved
+        self.fill_thyself()
+
+    def on_new  (self, evt):
+        print("TODO: got a 'new' button click")
+
+    def fill_thyself(self):
+
+        world = self.world
 
         self.host.SetValue(unicode(world.get("host")   ) or "")
         self.port.SetValue(    int(world.get("port")   ) or "")
-        self.conntype.SetSelection(world.get("conntype") or 0)
+
+        self.conntype.SetStringSelection(world.get("conntype") or "Direct")
         self.username.SetValue(    world.get("username") or "")
         self.password.SetValue(    world.get("password") or "")
 
@@ -186,7 +215,7 @@ class WorldsList(wx.Dialog):
         self.show_ssh_fields_if_appropriate()
 
     def show_ssh_fields_if_appropriate(self, evt = None):
-        to_show = self.conntype.GetSelection() == 2
+        to_show = self.conntype.GetSelection() == conntypes.index('SSH Fwd')
         self.ssh_username_label.Show(to_show)
         self.ssh_loc_host_label.Show(to_show)
         self.ssh_username.Show(to_show)
--- worlds.py
+++ worlds.py
@@ -2,51 +2,52 @@ import wx
 import re
 
 class World(dict):
-    _fields = '''
-        name host port user pass note type description
-        ssh_server ssh_user ssh_loc_host ssh_loc_port
-        ssh_rem_host ssh_rem_port
-    '''.split()
 
     _defaults = {
-        'port' : 7777,
-        'type' : 0,   # Socket
+        'port'     : '7777',
     }
 
     def __init__(self, data):
         data = data or World._defaults
-        for f in World._fields:
+        for f in data:
             if data.get(f) is not None: self[f] = data.get(f)
 
     def save(self):
         global _config
         worldname = re.sub(r'\W', '_', self.get('name'))
 
+        _config.DeleteGroup(worldname)
         _config.SetPath(worldname)
 
-        for f in World._fields:
-            if self.get(f) is not None: _config.Write(f, unicode(self.get(f)))
+        for f in self:
+            if self.get(f): _config.Write(f, unicode(self.get(f)))
 
         _config.SetPath('/')
+        _config.Flush()
 
 worlds    = {}
 _defaults = { }
 _config = wx.FileConfig(localFilename = '.wxpymoo_worlds')
 
+# loop worlds...
 g_more, worldname, g_index = _config.GetFirstGroup()
 while g_more:
     _config.SetPath(worldname)
 
     worlddata = {}
 
+    # loop data lines inside each world....
     e_more, dataname, e_index = _config.GetFirstEntry()
     while e_more:
         worlddata[dataname] = _config.Read(dataname)
         e_more, dataname, e_index = _config.GetNextEntry(e_index)
 
-    worlds[worldname] = World(worlddata)
-    g_more, worldname, g_index = _config.GetNextGroup(g_index)
+    # build the World object
+    worlds[worlddata['name']] = World(worlddata)
+
+    # carry on, back to the top for the next world
     _config.SetPath('/')
+    g_more, worldname, g_index = _config.GetNextGroup(g_index)
 
 else:
     import json
