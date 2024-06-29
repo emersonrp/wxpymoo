8304b1ae3cd47e1b06ef4c199158f101efe4ebc9
Russell Pickett <emerson@hayseed.net>
2024-06-29 13:54:21 -0400

    MSSP work;  saves into world now.  Much more work needed

--- window/msspinfo.py
+++ window/msspinfo.py
@@ -1,9 +1,5 @@
 import wx
 
-serverMsgAttr = wx.TextAttr(wx.Colour(128, 0, 0))
-clientMsgAttr = wx.TextAttr(wx.Colour(0,   0, 128))
-plainMsgAttr  = wx.TextAttr(wx.Colour(0,   0, 0))
-
 class MSSPInfo(wx.Dialog):
     def __init__(self, conn):
         worldname = conn.world.get('name')
@@ -12,13 +8,13 @@ class MSSPInfo(wx.Dialog):
         )
 
         self.connection = conn
-        self.output_pane = wx.StaticText(self)
+        self.output_list = wx.ListCtrl(self, style = wx.LC_REPORT)
 
         # TODO - the MSSP messages should live somewhere else, like in telnetiac.mssp or something
         self.mssp_msgs = {}
 
         sizer = wx.BoxSizer( wx.VERTICAL )
-        sizer.Add(self.output_pane, 1, wx.ALL|wx.GROW, 10)
+        sizer.Add(self.output_list, 1, wx.ALL|wx.GROW, 10)
         self.SetSizer(sizer)
 
         self.connection.status_bar.feature_icons['MSSP'].Bind(wx.EVT_LEFT_UP, self.toggle_visible)
@@ -26,23 +22,24 @@ class MSSPInfo(wx.Dialog):
         self.update()
 
     def toggle_visible(self, evt = None):
-        self.update()
-        self.Fit()
-        self.CenterOnParent()
-        self.Show(not self.IsShown())
+        if self.IsShown():
+            self.Hide()
+        else:
+            self.update()
+            self.Layout()
+            self.CenterOnParent()
+            self.Show()
         if evt: evt.Skip()
-
-    def Close(self, force = False):
-        self.toggle_visible()
 
     def add_message(self, msg):
         for k,v in msg.items():
             self.mssp_msgs[k] = v
-        self.update()
 
     def update(self):
-        output = ''
+        self.output_list.ClearAll()
+        self.output_list.AppendColumn("Variable")
+        self.output_list.AppendColumn("Value")
         for k in sorted(self.mssp_msgs.keys()):
-            output += f"{k}: {self.mssp_msgs[k]}\n"
-
-        self.output_pane.SetLabel(output)
+            self.output_list.Append([k, self.mssp_msgs[k]])
+        self.output_list.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        self.output_list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
